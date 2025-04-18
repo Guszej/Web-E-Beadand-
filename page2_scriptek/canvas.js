2531d89d2246b448a96dc143932d98de73eb6ae8
Guszej <vmark20408@gmail.com>
2025-04-18 13:26:37 +0200

    simítások és ajax

--- page2_scriptek/canvas.js
+++ page2_scriptek/canvas.js
@@ -1,21 +1,17 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
-// Négyzet rajzolása
 function drawSquare() {
-    // Véletlenszerű pozíciók és színek
     const x = Math.random() * canvas.width;
     const y = Math.random() * canvas.height;
-    const size = 50; // Négyzet mérete
+    const size = 50;
     const colors = ['#FF5733', '#33FF57', '#3357FF', '#FF33A8', '#FF9E33'];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
 
-    // Négyzet rajzolása
     ctx.fillStyle = randomColor;
     ctx.fillRect(x, y, size, size);
 }
 
-// Vászon törlése
 function clearCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
--- page2_scriptek/showSection.js
+++ page2_scriptek/showSection.js
@@ -1,9 +1,7 @@
 function showSection(sectionId) {
-    // Hide all sections
     document.querySelectorAll('.section').forEach(div => {
         div.classList.remove('active');
     });
 
-    // Show only the selected section
     document.getElementById(sectionId).classList.add('active');
 }
